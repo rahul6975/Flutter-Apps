d78ccea2ce616081748d9fd5a53f8a89d546bfd7
rahul6975 <rahulya7569@gmail.com>
2021-07-02 17:52:57 +0530

    flutter_music_player

---
+++ music_player/lib/MusicPlayer.dart
@@ -0,0 +1,109 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class MusicPlayer extends StatefulWidget {
+  @override
+  _MusicPlayerState createState() => _MusicPlayerState();
+}
+
+class _MusicPlayerState extends State<MusicPlayer> {
+  var isPlaying = "play";
+  var string = "";
+  AudioPlayer audioPlayer;
+  static AudioCache player = new AudioCache();
+  Future<void> play(var string) async {
+    this.string = string;
+    audioPlayer = await player.play("$string.mp3");
+    isPlaying = "pause";
+  }
+
+  void pause() {
+    audioPlayer.pause();
+    isPlaying = "play";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Image.asset('assets/brown.png', width: 200, height: 100),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  play("brown");
+                },
+                child: Text(
+                  "Brown Munde",
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Image.asset('assets/chahe.png', width: 200, height: 100),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  play("chahe");
+                },
+                child: Text(
+                  "Tuhje Kitna Chahe Or Hum",
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Image.asset('assets/kalank.png', width: 200, height: 100),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  play("kalank");
+                },
+                child: Text(
+                  "Kalank Title Track",
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+              ),
+            )
+          ],
+        ),
+        Divider(thickness: 2, color: Colors.black),
+        Padding(
+          padding: const EdgeInsets.only(top: 150),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: FlatButton(
+                      onPressed: () {
+                        pause();
+                        setState(() {
+                          Image.asset('assets/$isPlaying.png');
+                        });
+                      },
+                      child: Image.asset(
+                        'assets/$isPlaying.png',
+                        width: 50,
+                        height: 50,
+                      )))
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ music_player/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'MusicPlayer.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue,
+      body: MusicPlayer(),
+    ),
+  ));
+}
