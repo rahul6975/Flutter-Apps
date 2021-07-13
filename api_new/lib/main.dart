e896c53c9378d4adc870374fed70a0ad0001a53d
rahul6975 <rahulya7569@gmail.com>
2021-07-13 12:41:20 +0530

    cross platform

--- api_new/lib/main.dart
+++ api_new/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
 import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 void main() {
   runApp(
     MaterialApp(
-      home: MyApp(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Cross Platform"),
+        ),
+        body: MyApp(),
+      ),
     ),
   );
 }
@@ -19,20 +23,71 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<String> fruits = ['Oranges', 'Apples', 'Bananas'];
+  String? cartItem = 'No Items';
+  List<DropdownMenuItem<String>> getItems() {
+    List<DropdownMenuItem<String>> items = [];
+    fruits.forEach((element) {
+      var eachItem = DropdownMenuItem(
+        child: Text('$element'),
+        value: element,
+      );
+      items.add(eachItem);
+    });
+    return items;
+  }
 
-  
+  DropdownButton getAndroiDropDown() {
+    return DropdownButton<String>(
+      value: 'Apples',
+      items: getItems(),
+      onChanged: (value) {
+        setState(() {
+          cartItem = value;
+        });
+      },
+    );
+  }
+
+  Widget getPlatformSpecific() {
+    if (Platform.isIOS) {
+      return getIosDropdown();
+    } else {
+      return getAndroiDropDown();
+    }
+  }
+
+  getIosDropdown() {
+    List<Widget> list = [];
+    return CupertinoPicker(
+        itemExtent: 30.0,
+        onSelectedItemChanged: (onSelectedItemChanged) {
+          fruits.forEach((element) {
+            list.add(Text('$element'));
+          });
+          setState(() {
+            cartItem = fruits[onSelectedItemChanged];
+          });
+        },
+        children: list);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Api"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Text("Hello"),
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: getPlatformSpecific(),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+            width: 30.0,
+          ),
+          Expanded(
+            child: Text('$cartItem'),
           ),
         ],
       ),
