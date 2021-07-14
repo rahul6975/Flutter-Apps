87cdfed0458cf701c51df50946cbf496c7bd828e
rahul6975 <rahulya7569@gmail.com>
2021-07-14 11:43:42 +0530

    animations

---
+++ animations/lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'Page2.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+      routes: {
+        'page2': (context) => Page2(),
+      },
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animations"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "page2");
+                  },
+                  child: Text("Go to second page"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- dio_api/lib/main.dart
+++ dio_api/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
 
 void main() {
   runApp(
@@ -22,7 +20,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dio Api'),
+        title: Text('Animations'),
       ),
       body: Column(
         children: <Widget>[],
