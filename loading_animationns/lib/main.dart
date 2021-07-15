469fc7e1bb670056003321b43d250bdd36ffa25a
rahul6975 <rahulya7569@gmail.com>
2021-07-15 09:33:31 +0530

    animations

--- loading_animationns/lib/main.dart
+++ loading_animationns/lib/main.dart
@@ -16,61 +16,102 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.amberAccent,
         appBar: AppBar(
           title: Text("Animations"),
         ),
-        body: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text("My trip to Kaula Lumpur"),
+        body: TextButton(
+          onPressed: () {},
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.white60,
+              child: SizedBox(
+                width: 300,
+                height: 400,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            "My trip to Kaula Lumpur",
+                            style: TextStyle(color: Colors.green, fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(
+                              "From :",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text("Mumbai, India",
+                              style:
+                                  TextStyle(color: Colors.green, fontSize: 20)),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text("Departure :",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 15)),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text("30th July 2021",
+                              style:
+                                  TextStyle(color: Colors.green, fontSize: 20)),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text("Return :",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 15)),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text("30th August 2021",
+                              style:
+                                  TextStyle(color: Colors.green, fontSize: 20)),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text("From"),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text("Mumbai, India"),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text("Departure"),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text("30th July 2021"),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text("Return"),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text("30th August 2021"),
-                ),
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
