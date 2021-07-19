921acae7bf5feda8ea74df46b0d6fa00278f7459
rahul6975 <rahulya7569@gmail.com>
2021-07-19 16:03:12 +0530

    flutter test

---
+++ final_test/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  List<String> fruits = ['Oranges', 'Apples', 'Bananas'];
+  String? cartItem = 'No Items';
+  AnimationController? controller;
+  double value = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        vsync: this, duration: Duration(seconds: 1), upperBound: 100);
+
+    controller?.forward();
+
+    controller?.addListener(() {
+      setState(() {
+        value = controller!.value;
+      });
+    });
+  }
+
+  List<DropdownMenuItem<String>> getItems() {
+    List<DropdownMenuItem<String>> items = [];
+    fruits.forEach((element) {
+      var eachItem = DropdownMenuItem(
+        child: Text('$element'),
+        value: element,
+      );
+      items.add(eachItem);
+    });
+    return items;
+  }
+
+  DropdownButton getAndroidDropDown() {
+    return DropdownButton<String>(
+      value: 'Apples',
+      items: getItems(),
+      onChanged: (value) {
+        setState(() {
+          cartItem = value;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
